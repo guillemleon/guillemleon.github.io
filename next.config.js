45aa2d91e6fa3be0c6f06c812eed8181ed68c932
human-reborn <humanreborninfo@gmail.com>
2023-06-18 12:21:57 +0200

    removed experimental output

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  trailingSlash: true,
-  output: "export",
-  images: { unoptimized: true },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
