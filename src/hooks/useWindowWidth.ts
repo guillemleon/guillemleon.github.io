bc89ae23548b9dad124dec950da15d71ecef762f
human-reborn <humanreborninfo@gmail.com>
2023-06-18 11:56:58 +0200

    fixed window is undefined error on build

--- src/hooks/useWindowWidth.ts
+++ src/hooks/useWindowWidth.ts
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
 
+const isBrowser = () => typeof window !== "undefined";
+
 // Hook
 export function useWindowSize() {
   // Initialize state with undefined width/height so server and client renders match
   // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
   const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: isBrowser() ? window.innerWidth : 1920,
+    height: isBrowser() ? window.innerHeight : 1440,
   });
 
   useEffect(() => {
@@ -15,13 +17,13 @@ export function useWindowSize() {
     function handleResize() {
       // Set window width/height to state
       setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
+        width: isBrowser() ? window.innerWidth : 1920,
+        height: isBrowser() ? window.innerHeight : 1440,
       });
     }
 
     // Add event listener
-    window.addEventListener("resize", handleResize);
+    isBrowser() && window.addEventListener("resize", handleResize);
 
     // Call handler right away so state gets updated with initial window size
     handleResize();
